3d0b0a3628f97619d8188a7166707218492b9b7e
jiahuan <jiahuan.shen@capgemini.com>
2020-07-15 13:51:05 +0800

    sam v2

--- src/test/java/cn/mac/testConflit.java
+++ src/test/java/cn/mac/testConflit.java
@@ -10,6 +10,6 @@ public class testConflit {
     public static void main(String[] args) {
         System.out.println("2222");
         System.out.println("confict");
-        System.out.println("3333");
+        System.out.println("4444");
     }
 }
