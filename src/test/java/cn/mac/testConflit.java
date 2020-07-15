04ff0627b61852da9e8986e2ed1402e8d7c5efa2
jiahuan <jiahuan.shen@capgemini.com>
2020-07-15 13:31:38 +0800

    v1

---
+++ src/test/java/cn/mac/testConflit.java
@@ -0,0 +1,15 @@
+package cn.mac;
+
+/**
+ * @author shen jiahuan
+ * @version 1.0
+ * @date 2020/7/15 1:27 PM
+ */
+public class testConflit {
+
+    public static void main(String[] args) {
+        System.out.println("2222");
+        System.out.println("confict");
+        System.out.println("3333");
+    }
+}
